2c375f84c663469a8929c63f0aae73fa2c29d87b
Dmitrii Ermakov <dmitrii.ermakov@maxiv.lu.se>
2023-11-23 11:12:52 +0100

    Copy OIDC client settings on startup
    Signed-off-by: Dmitrii Ermakov <dmitrii.ermakov@maxiv.lu.se>

--- internal/auth/oidc_clients_store.go
+++ internal/auth/oidc_clients_store.go
@@ -21,7 +21,18 @@ type StaticOIDCClientsStore struct {
 }
 
 func NewStaticOIDCClientStore(config map[string]OIDCClientConfig) *StaticOIDCClientsStore {
-	return &StaticOIDCClientsStore{clients: config}
+	// Copy data.
+	clients := make(map[string]OIDCClientConfig, len(config))
+
+	for k, v := range config {
+		clients[k] = OIDCClientConfig{
+			ClientID:     v.ClientID,
+			ClientSecret: v.ClientSecret,
+			RedirectURL:  v.RedirectURL,
+		}
+	}
+
+	return &StaticOIDCClientsStore{clients: clients}
 }
 
 func NewEmptyStaticOIDCClientStore() *StaticOIDCClientsStore {
@@ -37,10 +48,10 @@ func (ocf *StaticOIDCClientsStore) GetClient(domain string) (*OIDCClientConfig, 
 
 func (ocf *StaticOIDCClientsStore) AddClient(domain string, clientid string, clientsecret string, redirecturl string) {
 	if _, ok := ocf.clients[domain]; !ok {
-		ocf.clients[strings.Clone(domain)] = OIDCClientConfig {
-			ClientID: strings.Clone(clientid),
+		ocf.clients[strings.Clone(domain)] = OIDCClientConfig{
+			ClientID:     strings.Clone(clientid),
 			ClientSecret: strings.Clone(clientsecret),
-			RedirectURL: strings.Clone(redirecturl),
+			RedirectURL:  strings.Clone(redirecturl),
 		}
 	}
 }
